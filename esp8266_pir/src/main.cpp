02f3f1dc0cae9d0b6d6e489f4d2381ca0808a52b
hongthiet1994 <hongthiet1994@gmail.com>
2022-01-12 16:15:04 +0700

    init code

---
+++ esp8266_pir/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#define PIN_PIR       D2
+#define PIN_RELAY     D5
+
+void setup() 
+{
+  Serial.begin(115200);
+  pinMode(PIN_PIR,INPUT);  // set pin 7 input
+  pinMode(PIN_RELAY,OUTPUT); // set pin 2 output
+  digitalWrite(PIN_RELAY,LOW); // turn off relay
+}
+
+void loop() 
+{
+  if (digitalRead(PIN_PIR) == true)
+  {
+    Serial.println("have motion");
+    digitalWrite(PIN_RELAY,HIGH);
+    delay(5000); // turn on light 5s
+    digitalWrite(PIN_RELAY,LOW);
+  }  
+}
